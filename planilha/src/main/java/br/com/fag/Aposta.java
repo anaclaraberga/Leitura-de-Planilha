5f0056a78b22492c6d6a245dfe0b36352d4e9bfe
Ana Clara Bergamini Delgado <ana4ew.br@gmail.com>
2023-09-26 08:05:07 -0300

    classpath e pacotes java

---
+++ planilha/src/main/java/br/com/fag/Aposta.java
@@ -0,0 +1,27 @@
+package br.com.fag;
+
+import java.util.Date;
+
+public class Aposta {
+  private int concurso;
+  private Date data;
+  private int bola1;
+  private int bola2;
+  private int bola3;
+  private int bola4;
+  private int bola5;
+  private int bola6;
+  private double ganhadoreSeisAcertos;
+  private double ganhadoreCincoAcertos;
+  private double ganhadoreQuatroAcertos;
+  private double rateioSeisAcertos;
+  private double rateioCincoAcertos;
+  private double rateioQuatroAcertos;
+  private String cidade;
+  private double acumuladosSeisAcertos;
+  private double arrecadacaoTotal;
+  private double estimativaPremio;
+  private double acumuladoSorteio;
+  private String observacao;
+
+}
---
+++ planilha/src/main/java/br/com/fag/GerenciadorGanhadores.java
@@ -0,0 +1,10 @@
+package br.com.fag;
+
+import java.util.List;
+
+public class GerenciadorGanhadores {
+  
+  public List<Ganhadores> criar() {
+    return null;
+  }
+}
