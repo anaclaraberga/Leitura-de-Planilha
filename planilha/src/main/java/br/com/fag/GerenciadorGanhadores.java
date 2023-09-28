cc218a1ed7cbe01925bd78cee81f1e65bf9c244b
Ana Clara Bergamini Delgado <ana4ew.br@gmail.com>
2023-09-27 21:56:52 -0300

    Inserção Lombok

--- planilha/src/main/java/br/com/fag/GerenciadorGanhadores.java
+++ planilha/src/main/java/br/com/fag/GerenciadorGanhadores.java
@@ -2,9 +2,13 @@ package br.com.fag;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 
+import org.apache.commons.collections.IteratorUtils;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
 import lombok.Cleanup;
@@ -16,10 +20,17 @@ public class GerenciadorGanhadores {
     //lendo os ganhadores
     @Cleanup FileInputStream file = new FileInputStream("src\\main\\resources\\Mega-Sena.xlsx");
 
+    //lendo a aba
     Workbook workbook = new HSSFWorkbook(file);
 
-    workbook.getSheetAt(0);
+    Sheet sheet = workbook.getSheetAt(0);
+
+    List<Row> rows = (List<Row>) toList(sheet.iterator());
 
     return null;
+  }
+  
+  public List<?> toList(Iterator<?> iterator) {
+    return IteratorUtils.toList(iterator);
   }
 }
